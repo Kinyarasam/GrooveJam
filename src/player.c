4b039f5cd87a4734240fbdee0223823cbde50e23
kinyarasam <skinyara.30@gmail.com>
2024-07-15 08:36:47 +0300

    GrooveJam

--- src/player.c
+++ src/player.c
@@ -1,5 +1,10 @@
 #include "player.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ao/ao.h>
+#include <mpg123.h>
+
+#define BITS 8
 
 /**
  * play_music - Play an MP3 file.
@@ -9,5 +14,49 @@
  * MP3 file, playing it through the speakers.
  */
 void play_music(const char *filename) {
-    printf("%s\n", filename);
+    mpg123_handle *mh;
+    unsigned char *buffer;
+    size_t buffer_size;
+    size_t done;
+    int err;
+    int driver;
+    ao_device *dev;
+    ao_sample_format format;
+    int channels, encoding;
+    long rate;
+
+    /* Initializations */
+    ao_initialize();
+    driver = ao_default_driver_id();
+    mpg123_init();
+    mh = mpg123_new(NULL, &err);
+    buffer_size = mpg123_outblock(mh);
+    buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
+
+    /* Open the mp3 file */
+    mpg123_open(mh, filename);
+    mpg123_getformat(mh, &rate, &channels, &encoding);
+
+    /* Set the output format */
+    format.bits = mpg123_encsize(encoding) * BITS;
+    format.rate = rate;
+    format.channels = channels;
+    format.byte_format = AO_FMT_NATIVE;
+    format.matrix = 0;
+
+    /* Open the audio device */
+    dev = ao_open_live(driver, &format, NULL);
+
+    /* Decode and play */
+    while(mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK) {
+        ao_play(dev, (char *)buffer, done);
+    }
+
+    /* Clean up */
+    free(buffer);
+    ao_close(dev);
+    mpg123_close(mh);
+    mpg123_delete(mh);
+    mpg123_exit();
+    ao_shutdown();
 }
